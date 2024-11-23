9a787cb52003b9b93760274459b68aa3762820ad
Cell1729 <sabax256256@gmail.com>
2024-11-23 22:20:50 +0900

    アクセスしたときのhtml画面の作成 データベース構造のみ作成

--- EnglishWordTest/models.py
+++ EnglishWordTest/models.py
@@ -1,3 +1,69 @@
 from django.db import models
 
-# Create your models here.
+class JuniorHighEnglish1000(models.Model):
+    id = models.IntegerField(primary_key=True)  # idフィールドを明示的に追加
+    word = models.CharField(max_length=100)
+    meaning = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return f'{self.word} - {self.meaning}'
+
+class SystemEnglish(models.Model):
+    id = models.IntegerField(primary_key=True)  # idフィールドを明示的に追加
+    word = models.CharField(max_length=100)
+    meaning = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return f'{self.word} - {self.meaning}'
+
+class Target1900(models.Model):
+    id = models.IntegerField(primary_key=True)  # idフィールドを明示的に追加
+    word = models.CharField(max_length=100)
+    meaning = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return f'{self.word} - {self.meaning}'
+
+class DeruJun5(models.Model):
+    id = models.IntegerField(primary_key=True)  # idフィールドを明示的に追加
+    word = models.CharField(max_length=100)
+    meaning = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return f'{self.word} - {self.meaning}'
+
+class DeruJun4(models.Model):
+    id = models.IntegerField(primary_key=True)  # idフィールドを明示的に追加
+    word = models.CharField(max_length=100)
+    meaning = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return f'{self.word} - {self.meaning}'
+
+class DeruJun3(models.Model):
+    id = models.IntegerField(primary_key=True)  # idフィールドを明示的に追加
+    word = models.CharField(max_length=100)
+    meaning = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return f'{self.word} - {self.meaning}'
+
+class DeruJunPre2(models.Model):
+    id = models.IntegerField(primary_key=True)  # idフィールドを明示的に追加
+    word = models.CharField(max_length=100)
+    meaning = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return f'{self.word} - {self.meaning}'
+
+class DeruJun2(models.Model):
+    id = models.IntegerField(primary_key=True)  # idフィールドを明示的に追加
+    word = models.CharField(max_length=100)
+    meaning = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return f'{self.word} - {self.meaning}'
+
+
+
+
--- EnglishWordTest/views.py
+++ EnglishWordTest/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import JuniorHighEnglish1000, SystemEnglish, Target1900, DeruJun5, DeruJun4, DeruJun3, DeruJunPre2, DeruJun2
 
-# Create your views here.
 def index(request):
-    return HttpResponse("hello")
+    return render(request, 'index.html')
+
+def search(request):
+    query = request.GET.get('q')
+    results = []
+    if query:
+        results.extend(JuniorHighEnglish1000.objects.filter(word__icontains=query) | JuniorHighEnglish1000.objects.filter(meaning__icontains=query))
+        results.extend(SystemEnglish.objects.filter(word__icontains=query) | SystemEnglish.objects.filter(meaning__icontains=query))
+        results.extend(Target1900.objects.filter(word__icontains=query) | Target1900.objects.filter(meaning__icontains=query))
+        results.extend(DeruJun5.objects.filter(word__icontains=query) | DeruJun5.objects.filter(meaning__icontains=query))
+        results.extend(DeruJun4.objects.filter(word__icontains=query) | DeruJun4.objects.filter(meaning__icontains=query))
+        results.extend(DeruJun3.objects.filter(word__icontains=query) | DeruJun3.objects.filter(meaning__icontains=query))
+        results.extend(DeruJunPre2.objects.filter(word__icontains=query) | DeruJunPre2.objects.filter(meaning__icontains=query))
+        results.extend(DeruJun2.objects.filter(word__icontains=query) | DeruJun2.objects.filter(meaning__icontains=query))
+    return render(request, 'search_results.html', {'results': results})
+
+
