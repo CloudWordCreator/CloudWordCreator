03e7d82210bc5c22a967c68a5f2620b08fababb4
Cell1729 <sabax256256@gmail.com>
2024-11-24 11:52:33 +0900

    update データベースに単語情報の登録 検索部分の細かい修正

--- CloudWordCreator/settings.py
+++ CloudWordCreator/settings.py
@@ -116,7 +116,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+# settings.py
+
+# 静的ファイルのURL
+STATIC_URL = '/static/'
+
+# 静的ファイルのディレクトリ
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- EnglishWordTest/views.py
+++ EnglishWordTest/views.py
@@ -8,14 +8,12 @@ def search(request):
     query = request.GET.get('q')
     results = []
     if query:
-        results.extend(JuniorHighEnglish1000.objects.filter(word__icontains=query) | JuniorHighEnglish1000.objects.filter(meaning__icontains=query))
-        results.extend(SystemEnglish.objects.filter(word__icontains=query) | SystemEnglish.objects.filter(meaning__icontains=query))
-        results.extend(Target1900.objects.filter(word__icontains=query) | Target1900.objects.filter(meaning__icontains=query))
-        results.extend(DeruJun5.objects.filter(word__icontains=query) | DeruJun5.objects.filter(meaning__icontains=query))
-        results.extend(DeruJun4.objects.filter(word__icontains=query) | DeruJun4.objects.filter(meaning__icontains=query))
-        results.extend(DeruJun3.objects.filter(word__icontains=query) | DeruJun3.objects.filter(meaning__icontains=query))
-        results.extend(DeruJunPre2.objects.filter(word__icontains=query) | DeruJunPre2.objects.filter(meaning__icontains=query))
-        results.extend(DeruJun2.objects.filter(word__icontains=query) | DeruJun2.objects.filter(meaning__icontains=query))
+        results.extend([{'text': '中学英単語1000', 'data': obj} for obj in JuniorHighEnglish1000.objects.filter(word__icontains=query) | JuniorHighEnglish1000.objects.filter(meaning__icontains=query)])
+        results.extend([{'text': 'システム英単語', 'data': obj} for obj in SystemEnglish.objects.filter(word__icontains=query) | SystemEnglish.objects.filter(meaning__icontains=query)])
+        results.extend([{'text': 'ターゲット1900', 'data': obj} for obj in Target1900.objects.filter(word__icontains=query) | Target1900.objects.filter(meaning__icontains=query)])
+        results.extend([{'text': 'でる順5級', 'data': obj} for obj in DeruJun5.objects.filter(word__icontains=query) | DeruJun5.objects.filter(meaning__icontains=query)])
+        results.extend([{'text': 'でる順4級', 'data': obj} for obj in DeruJun4.objects.filter(word__icontains=query) | DeruJun4.objects.filter(meaning__icontains=query)])
+        results.extend([{'text': 'でる順3級', 'data': obj} for obj in DeruJun3.objects.filter(word__icontains=query) | DeruJun3.objects.filter(meaning__icontains=query)])
+        results.extend([{'text': 'でる順準2級', 'data': obj} for obj in DeruJunPre2.objects.filter(word__icontains=query) | DeruJunPre2.objects.filter(meaning__icontains=query)])
+        results.extend([{'text': 'でる順2級', 'data': obj} for obj in DeruJun2.objects.filter(word__icontains=query) | DeruJun2.objects.filter(meaning__icontains=query)])
     return render(request, 'search_results.html', {'results': results})
-
-
