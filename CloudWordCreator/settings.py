0e7c19480036382826a17075a0746e91c719f723
Cell1729 <sabax256256@gmail.com>
2024-11-30 20:44:11 +0900

    update : 印刷用のCSSを追加
    最低限の機能の実装・デザインの調節が終了したのでデプロイしてもいい
    後は現場でどのような動作するか確認をする

--- CloudWordCreator/settings.py
+++ CloudWordCreator/settings.py
@@ -127,6 +127,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
+# 静的ファイルの収集先ディレクトリ
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
