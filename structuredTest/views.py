dc888f3b8b4219919ee7712a895b814330698513
Cell1729 <sabax256256@gmail.com>
2024-12-21 23:46:05 +0900

    update : テスト用紙のUIの変更
    主に子unitごとにテーブルを作成するように変更

--- structuredTest/views.py
+++ structuredTest/views.py
@@ -16,14 +16,18 @@ def create_test(request):
         # チェックされた単語IDとユニットIDを取得
         selected_word_ids = request.POST.getlist('selected_words')
         selected_unit_ids = request.POST.getlist('selected_units')
+        # 選択されたテキストを取得
+        selected_text_ids = request.POST.getlist('selected_texts')
         
-        # 選択された単語とユニットを取得
+        # 選択されたテキスト名、単語とユニットを取得
         selected_words = UnitWord.objects.filter(id__in=selected_word_ids)
         selected_units = Unit.objects.filter(id__in=selected_unit_ids)
+        selected_texts = Text.objects.filter(id__in=selected_text_ids)
         
         return render(request, 'test_result.html', {
             'words': selected_words,
             'units': selected_units,
+            'selected_texts': selected_texts,
         })
     
     return redirect('display_data')
