605d03b8c305fab9fe103adb632992d874855f5d
YYYY <ysaq3424@gmail.com>
2025-01-06 19:35:40 +0900

    fix : setting.html
    検索欄のcomboboxがJavaScriptと連動してしまい、フォームが予期しない値で制限される問題を修正
    検索欄のデザインを修正

--- staticfiles/flatTestMaker/js/setting_scripts.js
+++ staticfiles/flatTestMaker/js/setting_scripts.js
@@ -135,23 +135,3 @@ function removeWord(button) {
     button.parentElement.remove();
     validateForm();
 }
-
-// **タブ切り替え**
-function showTab(tabId) {
-    const tabs = document.querySelectorAll('.tab');
-    tabs.forEach(tab => {
-        tab.style.display = 'none';
-    });
-    document.getElementById(tabId).style.display = 'block';
-}
-
-document.getElementById('english-to-japanese-button').addEventListener('click', function() {
-    showTab('english-to-japanese-content');
-});
-
-document.getElementById('japanese-to-english-button').addEventListener('click', function() {
-    showTab('japanese-to-english-content');
-});
-
-// 初期表示を設定
-showTab('english-to-japanese-content');
