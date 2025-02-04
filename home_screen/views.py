043813bfa11ad9205b8d31a4df221f65b77333ca
Cell1729 <sabax256256@gmail.com>
2025-02-04 16:40:48 +0900

    update : home/report画面のcssの作成
    cssとjavaScriptsの作成
    URLにカーソルを合わせたときに教材一覧を表示させ、利便性の向上
    
    report画面では、校舎名は未入力でも送信できるように修正

--- home_screen/views.py
+++ home_screen/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from . import bug_report_writter
+from csvManager.models import UnitWord, NoUnitWord
 
 # Create your views here.
 # 暫定作るもの
@@ -9,7 +10,18 @@ from . import bug_report_writter
 ### gmailはGoogle SpreadSheetのシート2枚目にあるリストに通知をするようにする。
 
 def home(request):
-    return render(request, 'home.html')
+    # home画面では、URLにカーソルを合わせたときに対応している教材一覧を表示するためにデータベースから教材名を取得している。
+    # それぞれ分類するため2変数で取得している。
+    unit_words = UnitWord.objects.select_related('unit__text').all()
+    no_unit_words = NoUnitWord.objects.select_related('text').all()
+
+    flat_test_texts = set(nuw.text.name for nuw in no_unit_words)
+    structured_test_texts = set(uw.unit.text.name for uw in unit_words)
+
+    return render(request, 'home.html', {
+        'flat_test_texts': flat_test_texts,
+        'structured_test_texts': structured_test_texts
+    })
 
 def report(request):
     return render(request, 'report.html')
@@ -20,13 +32,13 @@ def submit_report(request):
         material = request.POST.get('material')
         details = request.POST.get('details')
 
-        report = {
+        report_content = {
             'schoolName' : school_name,
             'material' : material,
             'details' : details,
         }
 
-        sheet_writer = bug_report_writter.SpreadSheetWriter(report)
+        sheet_writer = bug_report_writter.SpreadSheetWriter(report_content)
         sheet_writer.write_report()
 
     return redirect('home')
