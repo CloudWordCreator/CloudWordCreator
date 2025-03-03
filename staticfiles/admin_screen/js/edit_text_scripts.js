c84abe660e4ccee01a96c420e4eb1b72be058528
YYYY <ysaq3424@gmail.com>
2025-03-03 11:20:35 +0900

    update : テスト結果のヘッダーを固定a
    印刷ボタンや、日英切り替えボタンを画面上部で固定表示するように修正

--- staticfiles/admin_screen/js/edit_text_scripts.js
+++ staticfiles/admin_screen/js/edit_text_scripts.js
@@ -1,9 +1,16 @@
 function searchText() {
     // ユーザーが入力した検索クエリを取得
     const query = document.getElementById('searchInput').value;
+    // 現在選択されている教材のIDを取得
+    const textId = document.getElementById('currentTextId').value;
+
+    if (!query) {
+        alert('検索ワードを入力してください');
+        return;
+    }
 
     // 非同期リクエスト (Fetch API)
-    fetch(`/eisai_admin/search_word/?word_query=${encodeURIComponent(query)}`, {  // 修正: word_query を使用
+    fetch(`/admin_screen/eisai_admin/search_word/?word_query=${encodeURIComponent(query)}&text_id=${encodeURIComponent(textId)}`, { // text_id をリクエストクエリに含める
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
@@ -24,8 +31,8 @@ function searchText() {
                 <ul>
                     ${data.results.map(word => `
                         <li>
-                            No: ${word.no} | 英語: ${word.english} | 日本語: ${word.japanese} |
-                            教材: ${word['unit__text__name'] || word['text__name']}
+                            No: ${word.no} | 英語: ${word.english} | 日本語: ${word.japanese} | ID : ${word.id} |
+                            <a href="/admin_screen/eisai_admin/edit_word/?word_id=${word.id}">編集</a>
                         </li>
                     `).join('')}
                 </ul>
@@ -39,4 +46,30 @@ function searchText() {
         console.error(error);
         alert('検索時に問題が発生しました。');
     });
+}
+
+function deleteWord(wordId, rowElement) {
+    const baseUrl = document.getElementById('deleteWordUrl').value; // テンプレートからURLを取得
+    if (confirm('この単語を削除してもよろしいですか？')) {
+        fetch(`${baseUrl}?word_id=` + wordId, {
+            method: 'GET',
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            if (data.message) {
+                alert(data.message);
+                rowElement.parentElement.parentElement.remove();
+            } else if (data.error) {
+                alert('削除に失敗しました: ' + data.error);
+            }
+        })
+        .catch(error => {
+            alert('エラーが発生しました: ' + error.message);
+        });
+    }
 }
