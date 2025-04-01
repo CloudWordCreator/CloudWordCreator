9b0e63d0333e4fb31fa8c9e43113e3adef962537
Cell1729 <sabax256256@gmail.com>
2025-04-01 12:34:30 +0900

    update : AWS Setupへのプルリクを通すためにuwsgi, commonの修正
    ALLOWED、uwsgiを修正

--- CloudWordCreator/settings/common.py
+++ CloudWordCreator/settings/common.py
@@ -31,8 +31,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = os.getenv('DEBUG')
 
-#ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS')]
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS'), "localhost", "127.0.0.1"]
+
 
 # Application definition
 
